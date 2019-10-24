7a66dd6e51c0303ee7a346d7b377bf09511cea4a
Liz Laffitte <liz.kossakowski@gmail.com>
2019-10-24 18:25:11 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,5 @@
 class Book
+  attr_accessor :author, :page_count
   def initialize(title)
     @title = title
   end
@@ -6,22 +7,7 @@ class Book
   def title
     @title
   end
-  
-  def author=(author)
-    @author = author
-  end
-  
-  def author
-    @author
-  end
-  
-  def page_count=(num)
-    @page_count=num
-  end
-  
-  def page_count
-    @page_count
-  end
+
   
   def genre=(genre)
     @genre = genre
