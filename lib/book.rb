4a6e5bfb57f14e557d5f7524affb437d3ff8d9b0
Liz Laffitte <liz.kossakowski@gmail.com>
2019-10-24 18:25:35 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -7,15 +7,6 @@ class Book
   def title
     @title
   end
-
-  
-  def genre=(genre)
-    @genre = genre
-  end
-  
-  def genre
-    @genre
-  end
   
   def turn_page
     puts "Flipping the page...wow, you read fast!"
